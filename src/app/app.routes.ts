bcbf824c733dd8095da7640f2c5be6ef1d3cd481
Benoit-Hayet <hayetbenoit@gmail.com>
2024-04-02 16:44:45 +0200

    Add navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ListKittenComponent } from './list-kitten/list-kitten.component';
+import { CreateKittenComponent } from './create-kitten/create-kitten.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',
+    component: ListKittenComponent
+},
+{path:'create-kitten',
+component: CreateKittenComponent},
+{path:'list-kitten',
+component: ListKittenComponent}];
